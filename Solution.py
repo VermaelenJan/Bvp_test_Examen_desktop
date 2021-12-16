bcc0c8e09a4e65767c424e56bb2eef5e62edbd99
Jan Vermaelen <jan.vermaelen@cs.kuleuven.be>
2021-12-16 09:29:48 +0100

    first commit

---
+++ Solution.py
@@ -0,0 +1,6 @@
+def some_function(x):
+    return 2*x
+
+
+def some_other_function(x):
+    return x*x
---
+++ Test.py
@@ -0,0 +1,6 @@
+from Solution import *
+
+if some_function(2) == some_other_function(2):
+    print("Tests succeeded")
+else:
+    print("Tests failed")
